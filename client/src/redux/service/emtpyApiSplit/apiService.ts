73a3b2d4fc353cc0378ba48032d1a6754c9ec28c
itszaki01 <itszacktouil@gmail.com>
2023-12-02 13:26:46 +0100

    update: baseapi url updated

--- client/src/redux/service/emtpyApiSplit/apiService.ts
+++ client/src/redux/service/emtpyApiSplit/apiService.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-export const baseUrl = 'http://localhost:8080'
+export const baseUrl = 'https://notaty-fullstack-a22730cb9363.herokuapp.com'
 export const apiService = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({baseUrl:baseUrl}),
